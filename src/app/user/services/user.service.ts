80726cab0f99a4f3242a2726522f38c612a74863
Markus Chiarot <cmarkus@hotmail.ch>
2020-04-05 16:33:44 +0200

    make userservice offline

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { User } from '../models/user';
 
 @Injectable({
@@ -9,14 +8,67 @@ import { User } from '../models/user';
 })
 export class UserService {
   public api = environment.API_URL + '/user';
-
-  constructor(private http: HttpClient) { }
+  public users: User[] = [
+    {
+      id: 1,
+      username: 'admin',
+      role: 'Administrator',
+      achievements: [],
+      tasks: [],
+      karma: {
+        experience: 2500,
+        maxExperience: 7000,
+        level: 6
+      }
+    },
+    {
+      id: 2,
+      username: 'Teacher1',
+      role: 'Teacher',
+      achievements: [],
+      tasks: [],
+      karma: {
+        experience: 100,
+        maxExperience: 3000,
+        level: 2
+      }
+    },
+    {
+      id: 3,
+      username: 'student2',
+      role: 'Student',
+      achievements: [],
+      tasks: [],
+      karma: {
+        experience: 6300,
+        maxExperience: 10000,
+        level: 9
+      }
+    },
+    {
+      id: 4,
+      username: 'student3',
+      role: 'Student',
+      achievements: [],
+      tasks: [],
+      karma: {
+        experience: 8500,
+        maxExperience: 10000,
+        level: 9
+      }
+    }
+  ];
 
   public getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.api);
+    return of(this.users);
   }
 
   public getUser(id: number): Observable<User> {
-    return this.http.get<User>(`${this.api}/${id}`);
+    return of(this.users.find(u => u.id === id));
+  }
+
+  public putUser(user: User): void {
+    const idx = this.users.findIndex(u => u.id === user.id);
+    this.users[idx] = user;
   }
 }
